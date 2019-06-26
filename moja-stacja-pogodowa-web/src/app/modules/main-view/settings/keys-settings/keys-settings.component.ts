dc3acab1112e221aab1343cd2759f875276f5f54
xAmaris <gabi97_97@o2.pl>
2019-06-26 22:45:33 +0200

    configs

--- moja-stacja-pogodowa-web/src/app/modules/main-view/settings/keys-settings/keys-settings.component.ts
+++ moja-stacja-pogodowa-web/src/app/modules/main-view/settings/keys-settings/keys-settings.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { WeatherApiKeys } from 'src/app/models/weather-api-keys.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-keys-settings',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./keys-settings.component.scss']
 })
 export class KeysSettingsComponent implements OnInit {
+  // tslint:disable-next-line: variable-name
+  private _weatherApiKeys: WeatherApiKeys;
+  apiKeysForm: FormGroup;
 
-  constructor() { }
+  @Input() set weatherApiKeys(weatherApiKeys: WeatherApiKeys) {
+    if (weatherApiKeys) {
+      if (!this.apiKeysForm) {
+        this.setWidgetForm();
+      }
+      this._weatherApiKeys = weatherApiKeys;
+      const apiKeyGroup = (this.apiKeysForm.controls.apiKeys as FormGroup)
+        .controls;
+      apiKeyGroup.OWMKey.setValue(weatherApiKeys.OWMKey);
+      apiKeyGroup.AWKey.setValue(weatherApiKeys.AWKey);
+      apiKeyGroup.WBKey.setValue(weatherApiKeys.WBKey);
+    }
+  }
+  get weatherApiKeys(): WeatherApiKeys {
+    return this._weatherApiKeys;
+  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.setWidgetForm();
   }
-
+  setWidgetForm() {
+    this.apiKeysForm = this.fb.group({
+      apiKeys: this.fb.group({
+        OWMKey: '',
+        AWKey: '',
+        WBKey: ''
+      })
+    });
+  }
+  onSubmit(): void {
+    if (!this.apiKeysForm.valid) {
+    } else {
+    }
+  }
 }
--- moja-stacja-pogodowa-web/src/app/modules/main-view/settings/widget-settings/widget-settings.component.ts
+++ moja-stacja-pogodowa-web/src/app/modules/main-view/settings/widget-settings/widget-settings.component.ts
@@ -8,43 +8,5 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./widget-settings.component.scss']
 })
 export class WidgetSettingsComponent implements OnInit {
-  // tslint:disable-next-line: variable-name
-  private _weatherApiKeys: WeatherApiKeys;
-  widgetForm: FormGroup;
-
-  @Input() set weatherApiKeys(weatherApiKeys: WeatherApiKeys) {
-    if (weatherApiKeys) {
-      if (!this.widgetForm) {
-        this.setWidgetForm();
-      }
-      this._weatherApiKeys = weatherApiKeys;
-      const apiKeyGroup = (this.widgetForm.controls.apiKeys as FormGroup)
-        .controls;
-      apiKeyGroup.OWMKey.setValue(weatherApiKeys.OWMKey);
-      apiKeyGroup.AWKey.setValue(weatherApiKeys.AWKey);
-      apiKeyGroup.WBKey.setValue(weatherApiKeys.WBKey);
-    }
-  }
-  get weatherApiKeys(): WeatherApiKeys {
-    return this._weatherApiKeys;
-  }
-  constructor(private fb: FormBuilder) {}
-
-  ngOnInit() {
-    this.setWidgetForm();
-  }
-  setWidgetForm() {
-    this.widgetForm = this.fb.group({
-      apiKeys: this.fb.group({
-        OWMKey: '',
-        AWKey: '',
-        WBKey: ''
-      })
-    });
-  }
-  onSubmit(): void {
-    if (!this.widgetForm.valid) {
-    } else {
-    }
-  }
+  ngOnInit() {}
 }
